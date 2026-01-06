0a65b0a8a5df2f6d43d1e9ca40310e949d4a3231
Jhon Vincent Colasi <Jhon_Vincent.Colasi@genesys.com>
2026-01-06 11:43:30 +0800

    Remove history files and terraform lock file
    - Removed .history/ folder containing IDE backup files
    - Removed .terraform.lock.hcl to allow flexible provider version management
    - Repository is now clean with only essential files

--- .history/modules/bot-flow/bot-flow_20251218141908.tf
+++
@@ -1,9 +0,0 @@
-resource "genesyscloud_flow" "bot_flow" {
-  filepath          = "${path.module}/MergeExternalContactBot.yaml"
-  file_content_hash = filesha256("${path.module}/MergeExternalContactBot.yaml")
-  substitutions = {
-    bot_name         = var.bot_name
-    division         = var.division
-    default_language = "en-us"
-  }
-}
